b2a888adf864cce423a1c56662402a485a06f99b
pjastr <24464854+pjastr@users.noreply.github.com>
2017-11-05 14:33:18 +0100

    Add files via upload

---
+++ ConsoleApplication49/ConsoleApplication49/Rower.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication49
+{
+    class Rower:Pojazd, IJazda, IMuzyka
+    {
+        public void Jedz()
+        {
+            Console.WriteLine("Jadę rowerem");
+        }
+
+
+
+        public void Klakson()
+        {
+            Console.WriteLine("dryń");
+        }
+    }
+}
---
+++ ConsoleApplication50/ConsoleApplication50/Osoba.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication50
+{
+    class Osoba: ISport, IMuzyka
+    {
+        //public string Gra()
+        //{
+        //    return "gra";
+        //}
+
+        string IMuzyka.Gra()
+        {
+            return "muzyka";
+        }
+
+        string ISport.Gra()
+        {
+            return "sport";
+        }
+    }
+}
